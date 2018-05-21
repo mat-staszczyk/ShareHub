69e08198c1e99eef0b1dfeedef54cb077cfd985b
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2018-05-21 19:53:49 +0200

    Add user AR validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :things, dependent: :destroy
   has_many :transactions_as_borrower, class_name: 'Transaction', foreign_key: 'borrower_id'
   has_many :transactions_as_lender, class_name: 'Transaction', foreign_key: 'lender_id'
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name,  presence: true, length: { maximum: 75 }
+  validates :nickname,   length: { maximum: 100 }
 end
