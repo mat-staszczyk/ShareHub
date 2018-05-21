e4511bd61d76b7324f6ad2254f3876595cb0e592
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2018-05-21 21:20:27 +0200

    Add new thing form

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -4,4 +4,26 @@ class ThingsController < ApplicationController
 
     render locals: { things: things }
   end
+
+  def new
+    thing = current_user.things.new
+
+    render locals: { thing: thing }
+  end
+
+  def create
+    thing = current_user.things.new(thing_params)
+
+    if thing.save
+      redirect_to root_path
+    else
+      render :new, locals: { thing: thing }
+    end
+  end
+
+  private
+
+  def thing_params
+    params.require(:thing).permit(:name, :visible, :description)
+  end
 end
