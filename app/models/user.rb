f21c2227c47f5b4417ace06bcddad4ce2636cdd2
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2018-05-21 19:06:13 +0200

    Add borrowing transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :things, dependent: :destroy
   has_many :borrowed_things, class_name: 'Thing', foreign_key: 'borrower_id'
+  has_many :things, dependent: :destroy
+  has_many :transactions_as_borrower, class_name: 'Transaction', foreign_key: 'borrower_id'
+  has_many :transactions_as_lender, class_name: 'Transaction', foreign_key: 'lender_id'
 end
