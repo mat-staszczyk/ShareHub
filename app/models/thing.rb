be1cd7902f31079cb12fc3941c510cd6d7cc9b3e
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2018-06-03 15:30:50 +0200

    Active Storage setup

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -3,4 +3,7 @@ class Thing < ApplicationRecord
   belongs_to :borrower, class_name: 'User', optional: true
 
   validates :name, presence: true, length: { maximum: 255 }
+
+  has_one_attached :header_image
+  has_many_attached :uploads
 end
