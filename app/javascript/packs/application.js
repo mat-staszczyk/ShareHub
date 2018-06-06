393873604a3adaffae30976abe95f6ddfb91d725
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2018-06-06 14:09:29 +0200

    Add direct uploads for thing attachments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
-import "./styles/app";
+import "./styles/app.scss";
 
 import Rails from "rails-ujs";
 import "bootstrap";
+
+import * as ActiveStorage from "activestorage";
+import "../utils/direct_uploads";
+
+ActiveStorage.start();
 
 Rails.start();
