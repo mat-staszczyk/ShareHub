0ec02b6d68a3b6a0c0104208968cf8c5cd2410e1
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2018-06-13 10:36:32 +0200

    Add rolify and user admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  rolify
 
   has_many :borrowed_things, class_name: 'Thing', foreign_key: 'borrower_id'
   has_many :things, dependent: :destroy
@@ -12,4 +13,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 75 }
   validates :nickname,   length: { maximum: 100 }
+
+  def admin?
+    has_role?(:admin)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_154846) do
+ActiveRecord::Schema.define(version: 2018_06_12_151506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,26 @@ ActiveRecord::Schema.define(version: 2018_06_11_154846) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_admins_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_admins_on_resource_type_and_resource_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
   create_table "things", force: :cascade do |t|
@@ -83,6 +103,22 @@ ActiveRecord::Schema.define(version: 2018_06_11_154846) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_admins", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "admin_id"
+    t.index ["admin_id"], name: "index_users_admins_on_admin_id"
+    t.index ["user_id", "admin_id"], name: "index_users_admins_on_user_id_and_admin_id"
+    t.index ["user_id"], name: "index_users_admins_on_user_id"
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
   add_foreign_key "things", "users"
